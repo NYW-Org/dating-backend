2135eec68b091f20d938d7d09367f8d07f79870e
Naman-Rohilla <namanrohilla122@gmail.com>
2026-04-01 00:40:24 +0530

    @naman-rohilla | added spring boot in backend

---
+++ src/main/java/org/dating/DatingApplication.java
@@ -0,0 +1,11 @@
+package org.dating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DatingApplication.class, args);
+    }
+}
